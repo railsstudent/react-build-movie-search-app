85cdbf7e3242131c9cfe2c105caadf82f26c11ff
railsstudent <cleung334@gmail.com>
2020-06-10 14:09:42 +0800

    refactor: App component

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SearchMovies from './SearchMovies'
+
+export default function App() {
+  return (
+    <div class="container">
+      <h1 class="title">React Movie Search</h1>
+      <SearchMovies />
+    </div>
+  )
+}
--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import SearchMovies from './SearchMovies'
+import App from './App'
 import './style.css';
-
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'World updated'
-    };
-  }
-
-  render() {
-    return (
-      <div class="container">
-        <h1 class="title">React Movie Search</h1>
-        <SearchMovies />
-      </div>
-    );
-  }
-}
 
 render(<App />, document.getElementById('root'));
